36e5b15de46b9d46ae36fc021318ea2bdc10d563
khatabwedaa <khatabwedaa@users.noreply.github.com>
2024-03-20 21:23:28 +0000

    Fix code styling

--- tests/CompilesIconsTest.php
+++ tests/CompilesIconsTest.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Tests;
 
-use Orchestra\Testbench\TestCase;
 use BladeUI\Icons\BladeIconsServiceProvider;
 use Khatabwedaa\BladeCssIcons\BladeCssIconsServiceProvider;
+use Orchestra\Testbench\TestCase;
 
 class CompilesIconsTest extends TestCase
 {
@@ -15,7 +15,7 @@ class CompilesIconsTest extends TestCase
     {
         $result = svg('css-abstract')->toHtml();
 
-        $expected = <<<SVG
+        $expected = <<<'SVG'
 <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
   <path d="M5 5H19V19H16L16 8H5V5Z" fill="currentColor"/>
   <path fill-rule="evenodd" clip-rule="evenodd" d="M10 19C12.7614 19 15 16.7614 15 14C15 11.2386 12.7614 9 10 9C7.23858 9 5 11.2386 5 14C5 16.7614 7.23858 19 10 19ZM10 16C11.1046 16 12 15.1046 12 14C12 12.8954 11.1046 12 10 12C8.89543 12 8 12.8954 8 14C8 15.1046 8.89543 16 10 16Z" fill="currentColor"/>
@@ -30,7 +30,7 @@ SVG;
     {
         $result = svg('css-abstract', 'w-6 h-6 text-gray-500')->toHtml();
 
-        $expected = <<<SVG
+        $expected = <<<'SVG'
 <svg class="w-6 h-6 text-gray-500" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
   <path d="M5 5H19V19H16L16 8H5V5Z" fill="currentColor"/>
   <path fill-rule="evenodd" clip-rule="evenodd" d="M10 19C12.7614 19 15 16.7614 15 14C15 11.2386 12.7614 9 10 9C7.23858 9 5 11.2386 5 14C5 16.7614 7.23858 19 10 19ZM10 16C11.1046 16 12 15.1046 12 14C12 12.8954 11.1046 12 10 12C8.89543 12 8 12.8954 8 14C8 15.1046 8.89543 16 10 16Z" fill="currentColor"/>
@@ -45,7 +45,7 @@ SVG;
     {
         $result = svg('css-abstract', ['style' => 'color: #555'])->toHtml();
 
-        $expected = <<<SVG
+        $expected = <<<'SVG'
 <svg style="color: #555" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
   <path d="M5 5H19V19H16L16 8H5V5Z" fill="currentColor"/>
   <path fill-rule="evenodd" clip-rule="evenodd" d="M10 19C12.7614 19 15 16.7614 15 14C15 11.2386 12.7614 9 10 9C7.23858 9 5 11.2386 5 14C5 16.7614 7.23858 19 10 19ZM10 16C11.1046 16 12 15.1046 12 14C12 12.8954 11.1046 12 10 12C8.89543 12 8 12.8954 8 14C8 15.1046 8.89543 16 10 16Z" fill="currentColor"/>
